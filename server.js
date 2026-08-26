98518367af7a3424c5dd8095c57d0e8b5707aff1
studiotim14-commits <studiotim14@gmail.com>
2026-08-26 18:58:27 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
